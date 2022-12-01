d1dfe5bd9b47c4e385297ca37bad3ad0dbcdeed0
chdk0842 <chdk0842@naver.com>
2022-12-01 18:32:17 +0900

    up

---
+++ src/singtone/User.java
@@ -0,0 +1,17 @@
+package singtone;
+
+
+public class User {
+   //사용자 이름 필드
+   private String name;
+   
+   public User(String name) {
+	   this.name = name;
+   }
+   
+   public void print() {
+	   Printer printer = Printer.getPrinter();
+	   //print.tostring() 객체의 주소 보여줌
+	   System.out.println(this.name + "사용자가 사용하는 프린트 : " + printer.toString());
+   }
+}
